1540acc70a523797fe446b5101bf9b44b2442a3e
edvard_munch <arkchib@gmail.com>
2022-12-26 16:12:27 +0300

    Add movie name to a list of movies
    In addition to movie Id's that were being added before

--- all_films_rated_scraper.py
+++ all_films_rated_scraper.py
@@ -1,13 +1,12 @@
 import bs4
 
-FILENAME = 'files/all_movies_ranked'
 PARSER = 'html5lib'
 FILMS_ID = 'boxes'
 FILM_TAG_NAME = 'li'
 
 
-def list_of_movies():
-	with open(FILENAME) as fp:
+def list_of_movies(filename):
+	with open(filename) as fp:
 		soup = bs4.BeautifulSoup(fp, PARSER)
 
 	result_set = soup.find(id=FILMS_ID)
@@ -15,7 +14,14 @@ def list_of_movies():
 	movies = []
 	for res in result_set:
 		if res.name == FILM_TAG_NAME:
-			movies.append((res.contents[1].attrs['title']))
-			# print(f"{res.contents[1].contents[0]} {res.contents[1].attrs['title']}")
+			movie_data = [res.contents[1].attrs['title']]
+
+			if res.contents[1].string == None:
+				movie_data.append(f'{res.contents[1].contents[0]} {res.contents[1].contents[1].string}')
+
+			else:
+				movie_data.append(res.contents[1].string)
+
+			movies.append(movie_data)
 
 	return movies
